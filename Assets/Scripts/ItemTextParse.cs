9a2f3f6691af8ccd2230a01cec3ddb087f9c2fb5
Samuel DiBella <samuel.dibella@gmail.com>
2014-04-09 15:14:29 -0400

    atmosphere asset merging

---
+++ Assets/Scripts/ItemTextParse.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemTextParse : MonoBehaviour {
+
+	public TextAsset textFile; //assign in inspector
+	public Transform itemPrefab;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine( ParseAndGenerate() );
+	}
+	
+	IEnumerator ParseAndGenerate() {
+		Debug.Log(textFile.text);
+		
+		//clean windows line breaks
+		string cleanedTextData = textFile.text.Replace( "\r", "" );
+		string[] lines = cleanedTextData.Split( "\n" [0] );
+		
+		foreach (string line in lines) {
+			var newItem = Instantiate(itemPrefab, Random.insideUnitSphere * 10f, Quaternion.identity) as Transform;
+			
+			string[] data = line.Split( "," [0] );
+			newItem.name = data[0];
+			newItem.transform.localScale = new Vector3(1, float.Parse( data[1] ), 1);
+		}
+		
+		
+		
+		yield return 0;
+	}
+}
---
+++ Assets/Scripts/gasQualities.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gasQualities : MonoBehaviour {
+
+	public float oxygen;
+	public float butane;
+	
+	public bool isOxygenGen;
+	public bool isButaneGen;
+	
+	public float depleteRate = 3;
+	
+	Color standard = Color.white;
+	public Color butaneFull;// = Color.yellow;
+	public Color oxyFull;// = Color.blue;
+	
+	// Use this for initialization
+	void Start () {
+		oxygen = .5f;
+		butane = 0f;
+		
+		isOxygenGen = false;
+		isButaneGen = false;
+		
+		StartCoroutine( Deplete() );
+	}
+	
+	
+	void Update() {
+		//color update
+		renderer.material.color = Color.Lerp(standard, oxyFull, oxygen);
+		renderer.material.color = Color.Lerp(renderer.material.color, butaneFull, butane);
+		
+		Color color = renderer.material.color;
+		//uncomment for transparency
+		color.a = ((oxygen + butane) / 4) + .25f;
+		
+		renderer.material.color = color;
+	}
+	
+	
+	IEnumerator Deplete() {
+		while(true) {
+			if(oxygen > 0.05f) {
+				oxygen -= .05f;
+			}
+			
+			if(butane > 0.05f) {
+				butane -= .05f;
+			}
+			
+			yield return new WaitForSeconds(depleteRate);
+		}
+	}
+}
