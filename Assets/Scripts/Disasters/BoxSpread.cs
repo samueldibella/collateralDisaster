3327b8e237805c84cd4f07ad3f101ba20f48b776
Ben Daniszewski <bmd275@nyu.edu>
2014-04-23 15:31:47 -0400

    optimized a bit

--- Assets/Scripts/Disasters/BoxSpread.cs
+++ Assets/Scripts/Disasters/BoxSpread.cs
@@ -5,10 +5,16 @@ public class BoxSpread : MonoBehaviour {
 	private Bounds dimensions;
 	private float lastSpread;
 	private GameObject newCube = null;
-	public float spreadDelay;
+	private int cubesMade=0;
+	private GameObject[] childCubes;
+	public int spreadDelay;
 	public int gen;
 	public int genMax;
-
+	bool left =false;
+	bool right =false;
+	bool up =false;
+	bool down =false;
+	public bool spread=true;
 	// Use this for initialization
 	void Start () {
 		dimensions = gameObject.GetComponent<BoxCollider>().bounds;
@@ -18,43 +24,68 @@ public class BoxSpread : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > lastSpread + spreadDelay && gen < genMax) {
+		StartCoroutine ("Spread");
 
-			lastSpread = Time.time;
-			switch(Random.Range(0,4)){
-				case 0:{
-				if(Physics.OverlapSphere(transform.position + new Vector3(dimensions.size.x,0,0),0.01f).Length == 0)
-						newCube = Instantiate(gameObject, transform.position + new Vector3(dimensions.size.x,0,0), Quaternion.identity) as GameObject;
-					break;
-				}
-				
-				case 1:{
-				if(Physics.OverlapSphere(transform.position - new Vector3(dimensions.size.x,0,0),0.01f).Length == 0)
-						newCube = Instantiate(gameObject, transform.position - new Vector3(dimensions.size.x,0,0), Quaternion.identity) as GameObject;
-					break;
-				}
-				
-				case 2:{
-				if(Physics.OverlapSphere(transform.position + new Vector3(0,0,dimensions.size.z),0.01f).Length == 0)
-						newCube = Instantiate(gameObject, transform.position + new Vector3(0,0,dimensions.size.z), Quaternion.identity) as GameObject;
-					break;
-				}
-				
-				case 3:{
-				if(Physics.OverlapSphere(transform.position - new Vector3(0,0,dimensions.size.z),0.01f).Length == 0)
-						newCube = Instantiate(gameObject, transform.position - new Vector3(0,0,dimensions.size.z), Quaternion.identity) as GameObject;
-					break;
-				}
-
-			
-			}
-			
-			if (newCube != null){
-				newCube.GetComponent<BoxSpread>().gen = gen+1;
-			}
-			
-		}
 
 	
+	}
+	IEnumerator Spread(){
+		yield return new WaitForSeconds (Random.Range (0f, 2f));
+		while (true) {
+						if (up && down && left && right) {
+								spread = false;
+						}
+		
+						if (gen < genMax && spread) {
+			
+
+								switch (Random.Range (0, 4)) {
+								case 0:
+										{
+												if (Physics.Raycast (transform.position, transform.right, dimensions.size.x) == false)
+														newCube = Instantiate (gameObject, transform.position + new Vector3 (dimensions.size.x, 0, 0), Quaternion.identity) as GameObject;
+												else
+														right = true;
+												break;
+										}
+				
+								case 1:
+										{
+												if (Physics.Raycast (transform.position, transform.right * -1, dimensions.size.x) == false)
+														newCube = Instantiate (gameObject, transform.position - new Vector3 (dimensions.size.x, 0, 0), Quaternion.identity) as GameObject;
+												else
+														left = true;
+												break;
+										}
+				
+								case 2:
+										{
+												if (Physics.Raycast (transform.position, transform.forward, dimensions.size.x) == false)
+														newCube = Instantiate (gameObject, transform.position + new Vector3 (0, 0, dimensions.size.z), Quaternion.identity) as GameObject;
+												else
+														up = true;
+												break;
+										}
+				
+								case 3:
+										{
+												if (Physics.Raycast (transform.position, transform.forward * -1, dimensions.size.x) == false)
+														newCube = Instantiate (gameObject, transform.position - new Vector3 (0, 0, dimensions.size.z), Quaternion.identity) as GameObject;
+												else
+														down = true;
+												break;
+										}
+				
+				
+								}
+			
+								if (newCube != null) {
+										newCube.GetComponent<BoxSpread> ().gen = gen + 1;
+								}
+			
+						}
+						yield return new WaitForSeconds (0.5f);
+				}
+		
 	}
 }
